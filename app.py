be2fbb5acd8691cfead1a7f2326a03361e900002
CioVlad6 <85869123+CioVlad6@users.noreply.github.com>
2021-06-14 17:10:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 import socket
 
 redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
-app = Flask(_name_)
+app = Flask(__name__)
 @app.route("/")
 def hello():
 	try:
@@ -16,5 +16,5 @@ def hello():
 		"<b>Hostname:</b> {hostname}<br/>" \
 		"<b>Visits:</b> {visits}"
 	return html.format(name=os.getenv("NAME","world"), hostname=socket.gethostname(), visits=visits)
-if _name_ == "_main_":
+if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=80)
